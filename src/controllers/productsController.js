32820320ae8e2000819042bff65eccc77ce1efd5
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2021-09-27 13:10:25 -0300

    Update project to include partials, products, and users directory

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 const productsController = {
-  cart: (req, res) => res.sendFile(path.join(__dirname, '../views/productCart.html')),
-  detail: (req, res) => res.sendFile(path.join(__dirname, '../views/productDetail.html'))
+  cart: (req, res) => res.sendFile(path.join(__dirname, '../views/products/productCart.html')),
+  detail: (req, res) => res.sendFile(path.join(__dirname, '../views/products/productDetail.html'))
 }
 
 module.exports = productsController;
